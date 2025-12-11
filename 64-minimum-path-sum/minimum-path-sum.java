ddb22b322c345116835818023b7ec7e40ccae4bf
Tripti Chauhan <triptichauhan240@gmail.com>
2025-12-11 11:16:38 +0530

    Time: 4 ms (62.06%) | Memory: 50 MB (56.71%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,11 +3,11 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int dp[][]=new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
-            }
-        }
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
         return solve(0,0,grid,dp);
     }
 
@@ -19,14 +19,38 @@ class Solution {
         if(r==m-1 && c==n-1)
         return grid[r][c];
 
-        if(dp[r][c]!=-1)
-        return grid[r][c];
-        
-        int left=solve(r,c+1,grid,dp);
-        int right=solve(r+1,c,grid,dp);
 
-        grid[r][c]+=Math.min(left,right);
-        dp[r][c]=grid[r][c];
-        return dp[r][c];
+        // bottom-up
+       dp[0][0]=grid[0][0];
+       //rowFilling
+        for(int i=1;i<n;i++)
+        {
+            dp[0][i]=grid[0][i]+dp[0][i-1];
+        }
+        for(int i=1;i<m;i++)
+        {
+            dp[i][0]=grid[i][0]+dp[i-1][0];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+
+        return dp[m-1][n-1];
+
+
+
+        // top-down
+
+        // if(dp[r][c]!=-1)
+        // return dp[r][c];
+        
+        // int right=solve(r,c+1,grid,dp);
+        // int down=solve(r+1,c,grid,dp);
+
+        // grid[r][c]+=Math.min(down,right);
+        // dp[r][c]=grid[r][c];
+        // return dp[r][c];
     }
 }
