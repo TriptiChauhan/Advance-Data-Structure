9d6707116fa0da60879773a283d8b96dfb965f9a
Tripti Chauhan <triptichauhan240@gmail.com>
2025-12-17 10:56:16 +0530

    Time: 8 ms (31.35%) | Memory: 47.2 MB (46.75%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,29 +1,43 @@
+
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int n = matrix.length;
-        int[][] dp = new int[n][n];
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                dp[i][j] = Integer.MAX_VALUE;
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] dp = new int[m][n];
+
+        
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = matrix[0][j];
+        }
+
+       
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int down = dp[i - 1][j];
+                int downLeft = Integer.MAX_VALUE;
+                int downRight = Integer.MAX_VALUE;
+
+                if (j > 0) {
+                    downLeft = dp[i - 1][j - 1];
+                }
+
+                if (j < n - 1) {
+                    downRight = dp[i - 1][j + 1];
+                }
+
+                dp[i][j] = matrix[i][j] +
+                           Math.min(down, Math.min(downLeft, downRight));
             }
         }
-        int result = Integer.MAX_VALUE;
-        for(int j = 0; j < n; j++) {
-            result = Math.min(result, solve(0, j, matrix, dp));
-        }
-        return result;
-    }
 
-    int solve(int r, int c, int[][] matrix, int[][] dp) {
-        int n = matrix.length;
-        if(c < 0 || c >= n) return Integer.MAX_VALUE;
-        if(r == n - 1) return matrix[r][c];
-        if(dp[r][c] != Integer.MAX_VALUE) return dp[r][c];
-        int down = solve(r + 1, c, matrix, dp);
-        int downLeft = solve(r + 1, c - 1, matrix, dp);
-        int downRight = solve(r + 1, c + 1, matrix, dp);
-        dp[r][c] = matrix[r][c] + Math.min(down, Math.min(downLeft, downRight));
-        return dp[r][c];
+       
+        int ans = Integer.MAX_VALUE;
+        for (int j = 0; j < n; j++) {
+            ans = Math.min(ans, dp[m - 1][j]);
+        }
+
+        return ans;
     }
 }
-
