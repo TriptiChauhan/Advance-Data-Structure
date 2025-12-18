7ae0ad616594fb21b59c663b8abc8016cae3b37a
Tripti Chauhan <triptichauhan240@gmail.com>
2025-12-18 18:31:17 +0530

    Time: 389 ms (17.64%) | Memory: 169.2 MB (5.01%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,26 +1,27 @@
 class Solution {
     public String longestPalindrome(String s) {
         int len=s.length();
-        return solve(s,0,len);
+        Boolean dp[][]=new Boolean[len][len];
+        return solve(s,0,len,dp);
     }
 
 
 
 
-    String solve(String s,int i,int len){
+    String solve(String s,int i,int len,Boolean dp[][]){
         if(i==len)
         return "";
 
         String largest="";
         for(int j=i+1;j<=len;j++){
-           if(palin(s,i,j-1)){
+           if(palin(s,i,j-1,dp)){
            String curr=s.substring(i,j);
            if(curr.length()>largest.length())
            largest=curr;
            }
         }
 
-        String next =solve(s,i+1,len);
+        String next =solve(s,i+1,len,dp);
         if(largest.length()>next.length())
         return largest;
         else
@@ -28,14 +29,17 @@ class Solution {
        
     }
 
-    boolean palin(String s,int left,int right){
-      while(left<right){
-        if(s.charAt(left)!=s.charAt(right))
-        return false;
-        left++;
-        right--;
-      }
-      return true;
+    boolean palin(String s,int left,int right,Boolean dp[][]){
+     
+       if(left>=right)
+       return true;
+        if(dp[left][right]!=null)
+        return dp[left][right];
+
+    boolean result = (s.charAt(left) == s.charAt(right)) &&palin(s, left + 1, right - 1,dp);
+       dp[left][right]=result;
+      
+      return result;
 
     }
 }
