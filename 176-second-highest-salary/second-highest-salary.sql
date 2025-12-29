3d7d8451bb6f7ceb127efa9f1237534dd542850d
Tripti Chauhan <triptichauhan240@gmail.com>
2025-12-29 21:37:29 +0530

    Time: 233 ms (97.60%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -3,7 +3,10 @@
 -- from employee e1 inner join employee e2
 -- where e1.salary<e2.salary;
 
--- using subqueries
 
-select Max(salary) as SecondHighestSalary
-from employee where salary < (select MAX(salary) from employee );
+-- using subqueries
+-- select Max(salary) as SecondHighestSalary
+-- from employee where salary < (select MAX(salary) from employee );
+
+-- using order by
+select(select distinct salary from employee order by salary desc limit 1 offset 1) AS SecondHighestSalary;
