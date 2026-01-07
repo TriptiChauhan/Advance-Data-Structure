620a3dec9108a76242be12c2a3b4bec58c5ee213
Tripti Chauhan <triptichauhan240@gmail.com>
2026-01-07 10:47:06 +0530

    Time: 1 ms (17.77%) | Memory: 46.2 MB (16.17%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,33 +1,43 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        int dp[][]=new int[n+1][2];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<2;j++){
-                dp[i][j]=-1;
-            }
-        }
-        return solve(0,1,prices,dp);
+       
+        return solve(prices);
     }
 
-    int solve(int i,int flag,int prices[],int dp[][]){
-        if(i>=prices.length)
-        return 0;
 
-        if(dp[i][flag]!=-1)
-        return dp[i][flag];
-        int profit=0;
-        if(flag==1){
-            int buy=-prices[i]+solve(i+1,0,prices,dp);
-            int skip=solve(i+1,1,prices,dp);
-             profit=Math.max(buy,skip);
+    // Memoization
+    // int solve(int i,int flag,int prices[],int dp[][]){
+    //     if(i>=prices.length)
+    //     return 0;
+
+    //     if(dp[i][flag]!=-1)
+    //     return dp[i][flag];
+    //     int profit=0;
+    //     if(flag==1){
+    //         int buy=-prices[i]+solve(i+1,0,prices,dp);
+    //         int skip=solve(i+1,1,prices,dp);
+    //          profit=Math.max(buy,skip);
+    //     }
+    //     else{
+    //         int sell=prices[i]+solve(i+1,1,prices,dp);
+    //         int skip1=solve(i+1,0,prices,dp);
+    //          profit=Math.max(sell,skip1);
+    //     }
+    //     dp[i][flag]=profit;
+    //     return dp[i][flag];
+    // }
+
+
+    // tabulation
+    int solve(int prices[]){
+        int n=prices.length;
+         int dp[][]=new int[n+1][2];
+        for(int i=n-1;i>=0;i--){
+            dp[i][1]=Math.max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+
+            dp[i][0]=Math.max(prices[i]+dp[i+1][1],dp[i+1][0]);
         }
-        else{
-            int sell=prices[i]+solve(i+1,1,prices,dp);
-            int skip1=solve(i+1,0,prices,dp);
-             profit=Math.max(sell,skip1);
-        }
-        dp[i][flag]=profit;
-        return dp[i][flag];
+        return dp[0][1];
     }
 }
