0ff43beb6a4202ace54b625bf51e4275d4da2fac
Tripti Chauhan <triptichauhan240@gmail.com>
2026-03-24 12:19:12 +0530

    Time: 1 ms (87.66%) | Memory: 47.1 MB (91.54%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,38 +1,53 @@
-
-import java.util.*;
-
 class Solution {
+    static int par[], size[];
+    static int comp;
+
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        int[] visited = new int[n];
-        Arrays.fill(visited, -1);
 
-        int provinces = 0;
+        par = new int[n];
+        size = new int[n];
+        comp = n; 
 
         for (int i = 0; i < n; i++) {
-            if (visited[i] == -1) {
-                bfs(isConnected, visited, i, n);
-                provinces++;
-            }
+            par[i] = i;
+            size[i] = 1;
         }
 
-        return provinces;
-    }
-
-    void bfs(int[][] isConnected, int[] visited, int src, int n) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(src);
-        visited[src] = 1;
-
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-
-            for (int i = 0; i < n; i++) {
-                if (isConnected[curr][i] == 1 && visited[i] == -1) {
-                    queue.add(i);
-                    visited[i] = 1;
+      
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    union(i, j);
                 }
             }
         }
+
+        return comp;
+    }
+
+    // Find with path compression
+    int findpar(int node) {
+        if (node == par[node]) {
+            return node;
+        }
+        return par[node] = findpar(par[node]);
+    }
+
+    void union(int x, int y) {
+        int x_par = findpar(x);
+        int y_par = findpar(y);
+
+        if (x_par == y_par) return;
+
+        if (size[x_par] > size[y_par]) {
+            par[y_par] = x_par;
+            size[x_par] += size[y_par];
+        } else {
+            par[x_par] = y_par;
+            size[y_par] += size[x_par];
+        }
+
+        comp--; 
     }
 }
